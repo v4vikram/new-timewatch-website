1c9a91a72edc24490d4ae79a06800471ff98db1c
timewatch-dev <timewatchinfocomwebmail@gmail.com>
2025-12-12 18:19:17 +0530

    updated backend url

--- src/lib/axiosInstance.js
+++ src/lib/axiosInstance.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: "https://www.timewatchindia.com/api"
+    baseURL: process.env.NEXT_PUBLIC_API_URL || "https://preview.timewatchindia.com/api"
     // baseURL: "http://72.60.202.56:3001/api"
     // baseURL: "http://localhost:3001/api"
 }); 
